95ea30903e80b899ceaf50f3cbd055c02774ffee
XUJIY <70903847+XUJIY@users.noreply.github.com>
2022-07-10 10:01:08 +0800

    Update xf.sh

--- xf.sh
+++ xf.sh
@@ -60,7 +60,7 @@ cat <<EOF >/etc/v2ray/config.json
             "settings": {
                 "clients": [
                     {
-                        "id": "11111111-1111-1111-1111-111111111111",
+                        "id": "3E66A84A-C71B-79C8-5AEA-B3E6A1AADFEA",
                         "alterId": 0
                     }
                 ],
